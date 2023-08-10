1aed9e7684357ec410fc72d691ead0e544e6fde8
DanYunak <danyloyunak2006@gmail.com>
2023-08-10 19:47:53 +0300

    Changed routes

--- src/shared/api/axiosInstance.ts
+++ src/shared/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-    baseURL: 'https://gymwiki-server.onrender.com/',
+    baseURL: 'https://gymwiki.onrender.com/api/',
     withCredentials: true
 })
